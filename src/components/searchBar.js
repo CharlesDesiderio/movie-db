a4b1b813a50d477994eca74fda273a048a1153af
Charles Desiderio <cjdesi@gmail.com>
2019-08-06 22:02:16 +0800

    API request implementation

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Axios from 'axios';
 
 class SearchBar extends React.Component {
     state = {
-        input: ''
+        input: 'matrix',
+        searchResults: []
     }
 
+    componentDidMount() {
+        this.renderResults();
+    }
+
+    renderResults = () => {
+        let getUrl = 'https://api.themoviedb.org/3/search/movie';
+        let getApi = '?api_key=f78ec448fcd99f333c9145fd8c13eff0';
+        let getQuery = '&query=' + this.state.input;
+        let fullRequest = getUrl + getApi + getQuery;
+
+        Axios
+            .get(fullRequest)
+            .then(data => 
+               { this.setState({ searchResults: data.data.results });
+                console.log(this.state.searchResults);}
+                )
+
+            .catch(err => {
+                console.log(err);
+                //return null;
+            })
+    }
 
     handleChange = (props) => {
         this.setState({
@@ -16,9 +40,11 @@ class SearchBar extends React.Component {
         return (
             <div>
                 <input value={this.state.input} onChange={this.handleChange} />
+                <div>
+                    {this.state.searchResults.map(x => <div><h1>{x.original_title}</h1><img alt="poster" src={'http://image.tmdb.org/t/p/w185' + x.poster_path} /></div>)}
+                </div>
             </div>)
     }
 }
-
 
 export default SearchBar;
