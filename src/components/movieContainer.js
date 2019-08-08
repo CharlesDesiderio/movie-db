4606dffc2c93c8f95e82a9583997f82e75c17591
Charles Desiderio <cjdesi@gmail.com>
2019-08-08 09:17:11 +0800

    Links working!

--- src/components/movieContainer.js
+++ src/components/movieContainer.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 class MovieContainer extends React.Component {
     render() {
         return <div>
-            {this.props.results.map(x => <Link to={'/movie/'}><MovieCard key={x.id} title={x.original_title} imageUrl={x.poster_path} rating={x.vote_average} release={x.release_date} /></Link>)}
+            {this.props.results.map(x => <Link to={{pathname: '/movie/', state: { title: x.original_title, imageUrl: x.poster_path }}} ><MovieCard key={x.id} title={x.original_title} imageUrl={x.poster_path} rating={x.vote_average} release={x.release_date} /></Link>)}
         </div>
     }
 }
--- src/components/movieDetail.js
+++ src/components/movieDetail.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const MovieDetail = (props) => {
-    console.log(props);
+class MovieDetail extends React.Component {
+    //console.log(props);
+    render() {
     return (
         <div>
-            
+        <h1>{this.props.location.state.title}</h1>
+        <img alt="poster" src={'http://image.tmdb.org/t/p/w185' + this.props.location.state.imageUrl} />
         </div>
-    )
+    )}
 }
 
 export default MovieDetail;
