50bb97dd24e16273b0e7f4806bc003fdd3c71fa6
Charles Desiderio <cjdesi@gmail.com>
2019-08-10 22:06:08 +0800

    Updates

---
+++ precache-manifest.df6db9f9af7578fdf9dbf2809475c98b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c3e7a88126307eea2245a0e89d3ba8",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "5f351ac3e6f51964623d",
+    "url": "/movie-db/static/css/main.9118826e.chunk.css"
+  },
+  {
+    "revision": "6b54c88aa2570a14fd68",
+    "url": "/movie-db/static/js/2.2eb80455.chunk.js"
+  },
+  {
+    "revision": "5f351ac3e6f51964623d",
+    "url": "/movie-db/static/js/main.1e7e949a.chunk.js"
+  },
+  {
+    "revision": "6bb80631b382c7965b33",
+    "url": "/movie-db/static/js/runtime~main.5114c00d.js"
+  }
+]);
